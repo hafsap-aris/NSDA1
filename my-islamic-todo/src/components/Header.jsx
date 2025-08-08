36cd9ba0c4271ab5212f2bc8cbf979266c72a2c3
Hafsa <hafsafaris684@gmail.com>
2025-08-08 16:02:32 +0300

    in

--- my-islamic-todo/src/components/Header.jsx
+++ my-islamic-todo/src/components/Header.jsx
@@ -1,8 +1,17 @@
-import Navbar from "./Navbar";
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 function Header(){
-    return(
-        <Navbar/>
-    )
+return(
+    <>
+    <nav className="flex justify-between items-center px-6 py-4 bg-gray-900 text-white shadow-md">
+        <ul className="flex gap-6">
+            <li className="hover:text-gray-300 cursor-pointer"><Link to="/">Home</Link></li>
+            <li className="hover:text-gray-300 cursor-pointer"><Link to="/tasks">Tasks</Link></li>
+            <li className="hover:text-gray-300 cursor-pointer"><Link to="/explore">Explore</Link></li>
+            <li className="hover:text-gray-300 cursor-pointer"><Link to="/about">About</Link></li>
+        </ul>
+    </nav>
+    </>
+)
 }
 export default Header;
--- my-islamic-todo/src/pages/Home.jsx
+++ my-islamic-todo/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 
 export default function Home(){
     return(
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-100 to-blue-200 text-center px-4">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-100 to-blue-400 text-center px-4">
         <h1 className="text-5xl font-extrabold text-gray-800">Welcome to My Islamic To-Do App!</h1>
         </div>
     )
